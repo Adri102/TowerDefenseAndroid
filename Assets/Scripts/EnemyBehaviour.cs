e9132b2ff62c3dabb92032c6dccb84d1a07cac89
Adri102 <adri.arranz102@gmail.com>
2018-11-05 16:57:47 +0100

    Added the unity project

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour {
+
+    public GameObject enemy;
+    public GameObject[] points;
+
+    private int startPoint = 0;
+
+    [SerializeField]    int endPoint;
+    [SerializeField]    int currentPoint;
+
+    public float speed;
+    public float distance;
+    public float currentDistance;
+    public float travelTime;
+    public float lastTime;
+    public float currentTime;
+
+    // Use this for initialization
+    void Start ()
+    {
+        endPoint = points.Length;
+        currentPoint = 0;
+        distance = Vector2.Distance(enemy.transform.position, points[currentPoint].transform.position);
+        travelTime = distance / speed;
+        lastTime = Time.time;
+        RotateDirection();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        currentTime = Time.time - lastTime;
+        enemy.transform.position = Vector2.Lerp(enemy.transform.position, points[currentPoint].transform.position, currentTime/travelTime);
+        currentDistance = Vector2.Distance(enemy.transform.position, points[currentPoint].transform.position);
+
+        if((currentDistance < 0.1f))
+        {
+            if(currentPoint < points.Length - 1)
+            {
+                Debug.Log("point");
+                currentPoint++;
+                lastTime = Time.time;
+                RotateDirection();
+                distance = Vector3.Distance(enemy.transform.position, points[currentPoint].transform.position);
+            }
+            else
+            {
+                Destroy(enemy);
+            }
+
+        }
+
+	}
+
+    //
+    private void RotateDirection()
+    {
+        // Double check, was giving erros dunno why
+        if(currentPoint < points.Length - 1)
+        {
+            // Difference between point and enemy
+            Vector3 diff = (points[currentPoint].transform.position - enemy.transform.position);
+
+            // ATan2 Returns the angle in radians whose Tan is y/x, ignoring negatives numbers or /0 errors
+            float angle = Mathf.Atan2(diff.y, diff.x);
+
+            // Set rotation. 
+            enemy.transform.rotation = Quaternion.Euler(0f, 0f, angle * Mathf.Rad2Deg);
+        }
+
+    }
+}
---
+++ Assets/Scripts/EnemySpawnBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnBehaviour : MonoBehaviour {
+
+    public GameObject enemy;
+    public GameObject[] points;
+
+    // Use this for initialization
+    void Start ()
+    {
+        enemy.GetComponent<EnemyBehaviour>().points = points;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)) Instantiate(enemy, this.transform);
+	}
+}
