7ee88a070c80f2678439341c9483cd4c57c8895d
Adri102 <adri.arranz102@gmail.com>
2018-11-29 19:00:05 +0100

    Added tower building

---
+++ Assets/Scripts/TowerBuild.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerBuild : MonoBehaviour {
+
+    public GameObject tower;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+	}
+
+    void OnMouseUp()
+    {
+        GameObject newTower = Instantiate(tower);
+
+        newTower.transform.localPosition = new Vector3(this.transform.position.x, this.transform.position.y, 0);
+
+        newTower.transform.parent = null;
+
+        Destroy(this.gameObject);
+    }
+
+
+    // Esto no funciona
+    public void OnTouch()
+    {
+        GameObject newTower = Instantiate(tower);
+
+        newTower.transform.localPosition = new Vector3(this.transform.position.x, this.transform.position.y, 0);
+
+        newTower.transform.parent = null;
+
+        Destroy(this.gameObject);
+    }
+
+}
