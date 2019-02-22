aaf7a6423135e85c54605efcdab9c9098c2d28a0
Adri102 <adri.arranz102@gmail.com>
2019-02-22 16:56:13 +0100

    Tower Building
    -WIP- selection of tower works, but with some error in the position

---
+++ Assets/Scripts/ChooseTower.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChooseTower : MonoBehaviour {
+
+    public GameObject missileType;
+    public GameObject kineticType;
+    public GameObject laserType;
+    public TowerBuild builder;
+    [SerializeField] GameObject selectedType;
+
+    // Use this for initialization
+    void Start()
+    {
+        if(this.tag == "BuildMissile") selectedType = missileType;
+        else if(this.tag == "BuildKinetic") selectedType = kineticType;
+        else if(this.tag == "BuildLaser") selectedType = laserType;
+        else selectedType = null;
+    }
+    void OnMouseUp()
+    {
+        if(selectedType != null)
+        {
+            GameObject newTower = Instantiate(selectedType);
+
+            newTower.transform.localPosition = new Vector3(this.transform.position.x, this.transform.position.y, 0);
+
+            newTower.transform.parent = null;
+
+            builder.DeleteBuilding();
+        }
+    }
+
+
+    public void OnTouch()
+    {
+        if(selectedType != null)
+        {
+            GameObject newTower = Instantiate(selectedType);
+
+            newTower.transform.localPosition = new Vector3(this.transform.position.x, this.transform.position.y, 0);
+
+            newTower.transform.parent = null;
+            builder.DeleteBuilding();
+        }
+    }
+}
--- Assets/Scripts/TowerBuild.cs
+++ Assets/Scripts/TowerBuild.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class TowerBuild : MonoBehaviour {
 
-    public GameObject tower;
+    public GameObject missileTower;
+    public GameObject kineticTower;
+    public GameObject laserTower;
+    [SerializeField] bool building = false;
     // Use this for initialization
     void Start () {
 		
@@ -13,29 +16,49 @@ public class TowerBuild : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-
-	}
+        if(Input.GetMouseButtonDown(0))
+        {
+            /*building = false;
+            missileTower.SetActive(false);
+            kineticTower.SetActive(false);
+            laserTower.SetActive(false);*/
+        }
+    }
 
     void OnMouseUp()
     {
-        GameObject newTower = Instantiate(tower);
+        building = true;
+        missileTower.SetActive(true);
+        kineticTower.SetActive(true);
+        laserTower.SetActive(true);
+        /* GameObject newTower = Instantiate(tower);
 
-        newTower.transform.localPosition = new Vector3(this.transform.position.x, this.transform.position.y, 0);
+         newTower.transform.localPosition = new Vector3(this.transform.position.x, this.transform.position.y, 0);
 
-        newTower.transform.parent = null;
+         newTower.transform.parent = null;
 
-        Destroy(this.gameObject);
+         Destroy(this.gameObject);*/
     }
 
 
     public void OnTouch()
     {
+        building = true;
+        missileTower.SetActive(true);
+        kineticTower.SetActive(true);
+        laserTower.SetActive(true);
+        /*
         GameObject newTower = Instantiate(tower);
 
         newTower.transform.localPosition = new Vector3(this.transform.position.x, this.transform.position.y, 0);
 
         newTower.transform.parent = null;
 
+        Destroy(this.gameObject);*/
+    }
+
+    public void DeleteBuilding()
+    {
         Destroy(this.gameObject);
     }
 
